ee72f4cb5aff2b149c7b0da2ddfd30f73b3dfa2a
jatinluthra14 <jatinluthra14@gmail.com>
2016-04-23 22:16:36 +0530

    Update bash.rb
    Apt2

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,5 +1,6 @@
 bash "change system greeting" do
   user "root"
   code <<-EOH
-  apt-get update && apt-get upgrade && apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev libglapi-mesa libgl1-mesa-dri  EOH
-end
+  apt-get update && apt-get upgrade && apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev libglapi-mesa libgl1-mesa-dri
+  EOH
+  end
