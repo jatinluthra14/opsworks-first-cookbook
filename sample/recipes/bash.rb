c246870bfbb0797cee06a8c7a2e8dcac2184d1f6
jatinluthra14 <jatinluthra14@gmail.com>
2016-04-23 22:16:01 +0530

    Update bash.rb
    Apt

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,7 +1,5 @@
 bash "change system greeting" do
   user "root"
   code <<-EOH
-  echo "Hello OpsWorks World."
-  echo "It's working."
-  EOH
+  apt-get update && apt-get upgrade && apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev libglapi-mesa libgl1-mesa-dri  EOH
 end
